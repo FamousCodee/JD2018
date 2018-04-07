201bf4ff89c5fec7839caa0d1d5e1f9d4766506a
Vector <jianzai2311@163.com>
2018-04-08 00:20:56 +0800

    随便写写

--- src/main.py
+++ src/main.py
@@ -2,10 +2,16 @@
 import os
 import csv
 import pandas as pd
+import numpy as np 
+import lightgbm as lgb 
+import gc 
+from sklearn.model_selection import train_test_split, cross_val_score
 
 if os.path.exists("./data/PINGAN-2018-train_demo.csv"):
     path_train = "./data/PINGAN-2018-train_demo.csv"
+    PREDICT = False 
 else:
+    PREDICT = True 
     path_train = "/data/dm/train.csv"  # 训练文件
     path_test = "/data/dm/test.csv"  # 测试文件
     path_test_out = "model/"  # 预测结果输出路径为model/xx.csv,有且只能有一个文件并且是CSV格式。
@@ -18,8 +24,12 @@ def read_csv():
     """
     # for filename in os.listdir(path_train):
     tempdata = pd.read_csv(path_train)
+    test = pd.DataFrame()
+    if PREDICT:
+        test = pd.read_csv(path_test)
     tempdata.columns = ["TERMINALNO", "TIME", "TRIP_ID", "LONGITUDE", "LATITUDE", "DIRECTION", "HEIGHT", "SPEED",
                         "CALLSTATE", "Y"]
+    return tempdata, test
 
 
 def process():
@@ -52,4 +62,37 @@ if __name__ == "__main__":
     print("****************** start **********************")
     # 程序入口
     # process()
+    train, test = read_csv()
+    nrow_train = train.shape[0]
+    train = pd.concat([train, test], 0)
+    # print(df.head())
+    y_train = np.log1p(train["Y"])
+    X = train.drop('Y',axis=1, inplace=False)
+    # del train
+    # gc.collect()
+    x_train = X[:nrow_train]
+    x_test = X[nrow_train:]
+
+    train_X, valid_X, train_Y, valid_Y = train_test_split(x_train, y_train, test_size=0.1)
+    d_train = lgb.Dataset(train_X, label=train_Y)
+    d_valid = lgb.Dataset(valid_X, label=valid_Y)
+    watchlist = [d_train, d_valid]
+    params = {
+        'learning_rate': 0.75,
+        'application': 'regression',
+        'max_depth': 3,
+        'num_leaves': 100,
+        'verbosity': -1,
+        'metric': 'RMSE',
+    }
+
+    model = lgb.train(params, 
+    train_set=d_train, 
+    num_boost_round=2200,
+    valid_sets=watchlist,
+    early_stopping_rounds=50,
+    verbose_eval=100)
+
+if PREDICT:
+    preds = model.predict(x_test)
     
